scubes: fix errors calculation

splusdata/scubes/core.py
@@ -23,14 +23,14 @@ def _getdata_array(pathlist, ext):
 def _getheader_array(pathlist, ext):
     return np.array([fits.getheader(img, ext=ext) for img in pathlist])
 
-def _getval_array_mem(hdull, key):
-    return np.array([hdul.header.get(key) for hdul in hdull])
+def _getval_array_mem(hdull, key, ext):
+    return np.array([hdul[ext].header.get(key) for hdul in hdull])
 
-def _getdata_array_mem(hdull):
-    return np.array([hdul.data for hdul in hdull])
+def _getdata_array_mem(hdull, ext):
+    return np.array([hdul[ext].data for hdul in hdull])
 
-def _getheader_array_mem(hdull):
-    return np.array([hdul.header for hdul in hdull])
+def _getheader_array_mem(hdull, ext):
+    return np.array([hdul[ext].header for hdul in hdull])
 
 def _get_band_info_array(prop):
     return np.array([get_band_info(band)[prop] for band in BANDS])
@@ -48,13 +48,13 @@ class SCubes:
         self.size = size
 
     def _getval(self, obj, key, ext, mem=False):
-        return _getval_array_mem(obj, key) if mem else _getval_array(obj, key, ext)
+        return _getval_array_mem(obj, key, ext) if mem else _getval_array(obj, key, ext)
 
     def _getdata(self, obj, ext, mem=False):
-        return _getdata_array_mem(obj) if mem else _getdata_array(obj, ext)
+        return _getdata_array_mem(obj, ext) if mem else _getdata_array(obj, ext)
 
     def _getheader(self, obj, ext, mem=False):
-        return _getheader_array_mem(obj) if mem else _getheader_array(obj, ext)
+        return _getheader_array_mem(obj, ext) if mem else _getheader_array(obj, ext)
 
     def _download_calibrated_stamps(self, objname, outpath=None, force=False):
         images = []
@@ -79,7 +79,7 @@ class SCubes:
                 _ = self.conn.stamp(**kw_args)
                 wimages.append(kw_args['outfile'])
             else:
-                wimages.append(self.conn.stamp(**kw_args)[1])
+                wimages.append(self.conn.stamp(**kw_args))
         self.images = images
         self.wimages = wimages
 
@@ -104,8 +104,10 @@ class SCubes:
         gain__b = self._getval(self.images, 'GAIN', ext, mem=mem)
         gain__byx = gain__b[:, None, None]
         weidata__byx = np.abs(self._getdata(self.wimages, ext, mem=mem))
-        calib_data_err__byx = np.sqrt(1/weidata__byx + np.abs(calib_data__byx)/gain__byx)
-        efnu__byx = calib_data_err__byx*Jy2fnu*self.fnu_unit
+        zp_factor__byx = self._getdata(self.images, 2, mem=mem)
+        data__byx = np.abs(calib_data__byx/zp_factor__byx)
+        dataerr__byx = np.sqrt(1/weidata__byx + data__byx/gain__byx)
+        efnu__byx = dataerr__byx*zp_factor__byx*Jy2fnu*self.fnu_unit
         eflam__byx = scale*(efnu__byx*_c/self.wl__b[:, None, None]**2).to(self.flam_unit)
 
         self.flam__byx = flam__byx
@@ -158,7 +160,7 @@ class SCubes:
             if self.cubepath is not None:
                 hdr = fits.getheader(img, ext=ext)
             else:
-                hdr = img.header
+                hdr = img[ext].header
             key = [k for k in hdr.keys() if 'FWHMMEAN' in k]
             if len(key) == 1:
                 psffwhm__b.append(hdr.get(key[0]))
@@ -174,7 +176,7 @@ class SCubes:
         cube_prim_hdu.header['SIZE'] = (self.size, 'Side of the stamp in pixels')
         cube_prim_hdu.header['RA'] = self.ra
         cube_prim_hdu.header['DEC'] = self.dec
-        hdr = self.images[0].header if self.cubepath is None else fits.getheader(self.images[0], ext=1)
+        hdr = self.images[0][ext].header if self.cubepath is None else fits.getheader(self.images[0], ext=ext)
         cube_prim_hdu.header.update(self._stamp_WCS_to_cube_header(hdr))
         for key in ['X0TILE', 'X01TILE', 'Y0TILE', 'Y01TILE']:
             cube_prim_hdu.header[key] = hdr.get(key)

splusdata/scubes/read.py
@@ -32,7 +32,7 @@ def _parse_filters_tuple(f_tup, filters):
             i_f.append(f_tup)
     return i_f
 
-def make_RGB_tom(flux__lyx,
+def make_RGB_tom(flux__byx,
         rgb=(7, 5, 9), rgb_f=(1, 1, 1), 
         pminmax=(5, 95), im_max=255, 
         # astropy.visualization.make_lupton_rgb() input vars
@@ -50,7 +50,7 @@ def make_RGB_tom(flux__lyx,
                 i_f.append(f)
         else:
             i_f.append(f_tup)
-        C = copy(flux__lyx[i_f, :, :]).sum(axis=0)
+        C = copy(flux__byx[i_f, :, :]).sum(axis=0)
         # percentiles
         Cmin, Cmax = np.nanpercentile(C, pmin), np.nanpercentile(C, pmax)
         # calc color intensities
@@ -290,9 +290,9 @@ class read_scube:
         Computes the magnitude per square arcsecond and corresponding errors from the flux values.
         '''        
         a = 1/(2.997925e18*3631.0e-23*self.pixscale**2)
-        x = a*(self.flux__lyx*self.pivot_wave[:, np.newaxis, np.newaxis]**2)
-        self.mag_arcsec2__lyx = -2.5*np.log10(x)
-        self.emag_arcsec2__lyx = (2.5*np.log10(np.exp(1)))*self.eflux__lyx/self.flux__lyx
+        x = a*(self.flux__byx*self.pivot_wave[:, np.newaxis, np.newaxis]**2)
+        self.mag_arcsec2__byx = -2.5*np.log10(x)
+        self.emag_arcsec2__byx = (2.5*np.log10(np.exp(1)))*self.eflux__byx/self.flux__byx
 
     def _init(self):
         '''
@@ -303,6 +303,12 @@ class read_scube:
         self._init_centre()
         self._mag_values()
         self.pa, self.ba = 0, 1
+        self.set_ellipticity(self.pa, self.ba)
+        #self.pixel_distance__yx = get_image_distance(self.weimask__yx.shape, x0=self.x0, y0=self.y0, pa=self.pa, ba=self.ba)
+
+    def set_ellipticity(self, pa, ba):
+        self.pa = pa
+        self.ba = ba
         self.pixel_distance__yx = get_image_distance(self.weimask__yx.shape, x0=self.x0, y0=self.y0, pa=self.pa, ba=self.ba)
 
     def get_filter_i(self, filt):
@@ -364,13 +370,13 @@ class read_scube:
         i_rgb = [_parse_filters_tuple(x, self.filters) for x in rgb]
 
         return make_RGB_tom(
-            self.flux__lyx, rgb=i_rgb, rgb_f=rgb_f, 
+            self.flux__byx, rgb=i_rgb, rgb_f=rgb_f, 
             pminmax=pminmax, im_max=im_max, 
             minimum=minimum, Q=Q, stretch=stretch
         )
 
     @property
-    def weimask__lyx(self):
+    def weimask__byx(self):
         return np.broadcast_to(self.weimask__yx, (len(self.filters), self.size, self.size))
 
     @property
@@ -438,12 +444,12 @@ class read_scube:
         return self._hdulist['WEIMASK'].data
 
     @property 
-    def flux__lyx(self):
-        return self._hdulist['DATA'].data*self._hdulist['ERRORS'].header['BSCALE']
+    def flux__byx(self):
+        return self._hdulist['DATA'].data*self._hdulist['DATA'].header['BSCALE']
 
     @property 
-    def eflux__lyx(self):
-        return self._hdulist['ERRORS'].data*self._hdulist['ERRORS'].header['BSCALE']
+    def eflux__byx(self):
+        return self._hdulist['ERRORS'].data*self._hdulist['DATA'].header['BSCALE']
     
     @property
     def n_x(self):
@@ -458,13 +464,13 @@ class read_scube:
         return self.data_header['NAXIS3']
     
     @property
-    def SN__lyx(self):
-        return self.flux__lyx/self.eflux__lyx
+    def SN__byx(self):
+        return self.flux__byx/self.eflux__byx
 
     @property
-    def mag__lyx(self):
-        return self.mag_arcsec2__lyx
+    def mag__byx(self):
+        return self.mag_arcsec2__byx
 
     @property
-    def emag__lyx(self):
-        return self.emag_arcsec2__lyx
+    def emag__byx(self):
+        return self.emag_arcsec2__byx